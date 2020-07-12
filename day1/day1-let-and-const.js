1fc022be7c63e81489811299e51fcbc4a123c3d1
DariusRain <dariusrain.student@careerdevs.com>
2020-07-11 22:36:52 -0400

    Updated

---
+++ day1/day1-let-and-const.js
@@ -0,0 +1,7 @@
+const PI = Math.PI;
+
+// Print the area of the circle:
+console.log(PI * Math.pow(parseFloat(input), 2));
+
+// Print the perimeter of the circle:
+console.log(2 * PI * parseFloat(input));
